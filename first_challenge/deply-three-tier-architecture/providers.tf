851b5e732e7d9d8b743d5323f446917ac8bb74bf
lovemasta41 <46521394+lovemasta41@users.noreply.github.com>
2021-12-13 14:36:49 +0000

    Delete first_challenge/deply-three-tier-architecture directory

--- first_challenge/deply-three-tier-architecture/providers.tf
+++
@@ -1,5 +0,0 @@
-#using msi for access to azure
-provider "azure" {
-  subscription_id = var.subscription_id
-  use_msi = true
-}
--- first_challenge/deply-three-tier-architecture/terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.14"
-    required_providers {
-        azure = {
-            source = "hashicorp/azurerm"
-            version = "2.89.0"
-        }
-  }
-}
