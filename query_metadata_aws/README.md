7ea137faf450b541a0a108fb84e071a5301369d1
Masta, Love <love.masta@zurich.com>
2023-04-18 15:12:57 +0100

    updated code for second challenge

---
+++ query_metadata_aws/README.md
@@ -0,0 +1,13 @@
+# Problem Statement
+We need to write a code that will query the metadata of an instance within AWS or Azure or GCP and provide a json formatted output. 
+
+Bonus Points:  
+The code allows for a particular data key to be retrieved individually.
+
+# Solution
+A powershell script is created that prints meta data as json if no individual key is provided.   
+If a correct key is provided, its value will be printed ont he screen.  
+If incorrect key is provided then error will be printed on the scren stating provided key does not exists.  
+
+# Outputs
+Output for multiple test cases are attached in the form of an image.
--- query_metadata_aws/query_metadata.ps1
+++ query_metadata_aws/query_metadata.ps1
@@ -1,18 +1,129 @@
-$region = "us-east-1"
-$instance_id = (Invoke-RestMethod "http://169.254.169.254/latest/meta-data/instance-id")
+param(
+    $metaKey
+)
+
+##### region VARIABLES
+
 $url = "http://169.254.169.254/latest/meta-data/"
+$token = (Invoke-WebRequest "http://169.254.169.254/latest/api/token" -Method PUT -Headers @{"X-aws-ec2-metadata-token-ttl-seconds" =  "21600"}).content
 $headers = @{
-    "X-aws-ec2-metadata-token" = (Invoke-RestMethod "http://169.254.169.254/latest/api/token")
+    "X-aws-ec2-metadata-token" = $token
 }
 
-# Retrieve the metadata
-$response = Invoke-RestMethod -Method Get -Uri $url -Headers $headers
+##### region FUNCTIONS
+Function metadata(){  
 
-# Convert the metadata to a PowerShell object
-$metadata = $response | ConvertFrom-StringData
+    # Retrieve the metadata
+    $response = Invoke-WebRequest -Method Get -Uri $url -Headers $headers -UseBasicParsing -ErrorAction SilentlyContinue
+    $keys = $response.Content.Split([Environment]::NewLine)
 
-# Convert the metadata to JSON
-$metadata_json = $metadata | ConvertTo-Json
+    $dictTable = @()
 
-# Print the metadata JSON to the console
-Write-Host $metadata_json
+    foreach($key in $keys){
+    
+
+        if($key -like "*/"){
+        
+            $subkeys = (Invoke-WebRequest -method Get -uri "$url$key" -Headers $headers -UseBasicParsing -ErrorAction SilentlyContinue).Content.Split([Environment]::NewLine)
+            foreach($subkey in $subkeys){
+            
+                if($subkey -like "*/"){
+                
+                    $childKeys = (Invoke-WebRequest -method Get -uri "$url$key$subkey" -Headers $headers -UseBasicParsing -ErrorAction SilentlyContinue).Content.Split([Environment]::NewLine)
+                
+                    foreach($childkey in $childKeys){
+                    
+                        if($childkey -like "*/"){
+                        
+                            $subChildKeys = (Invoke-WebRequest -method Get -uri "$url$key$subkey$childKey" -Headers $headers -UseBasicParsing -ErrorAction SilentlyContinue).Content.Split([Environment]::NewLine)
+                            foreach($subchildKey in $subChildKeys){
+                            
+                                $subchildKeyResponse = Invoke-WebRequest -method Get -uri "$url$key$subkey$childkey$subchildKey" -Headers $headers -UseBasicParsing -ErrorAction SilentlyContinue
+                                $hashSubChildKeyObject = [pscustomobject]@{
+            
+                                    $subChildKey =  $subchildkeyResponse.Content
+                                }
+                                $dictTable += $hashSubChildKeyObject
+                            }
+                        }
+                        else{
+                        
+                            $childkeyResponse = Invoke-WebRequest -method Get -uri "$url$key$subkey$childkey" -Headers $headers -UseBasicParsing
+                            $hashChildKeyObject = [pscustomobject]@{
+            
+                                $childkey =  $childkeyResponse.Content
+                            }
+                            $dictTable += $hashChildKeyObject
+
+
+                        }
+                    }
+
+                }
+                else{
+                    
+                    $keyList = @()
+                    if($subkey -like "*=*"){
+                        
+                        $keyList += $subkey
+
+                    }
+                    else{
+
+                        $subkeyResponse = Invoke-WebRequest -method Get -uri "$url$key$subkey" -Headers $headers -UseBasicParsing -ErrorAction SilentlyContinue
+                        $subKeyObject = [pscustomobject]@{
+            
+                            $subkey =  $subkeyResponse.Content
+                        }
+                        $dictTable += $subKeyObject
+                    }
+
+                    if($keyList){
+                        
+                        $subKeyObject = [pscustomobject]@{
+            
+                            $key.split("/")[0] =  $keyList
+                        }
+                        $dictTable += $subKeyObject
+
+                    }
+                }
+            }
+            #>
+        }
+        else{
+        
+            $keyResponse = Invoke-WebRequest -method Get -uri "$url$key" -Headers $headers -UseBasicParsing
+        
+            $hashObject = [pscustomobject]@{
+            
+                $key =  $keyResponse.Content
+            }
+            $dictTable += $hashObject
+
+        }
+
+    }
+        
+return $dictTable
+
+}
+
+
+##### Region MAIN
+
+$metaData = metadata
+
+if($metaKey){
+
+    if($metaData.$metaKey){    
+        $metaData.$metaKey 
+    }
+    else{
+        "Provided key does not exist."
+    }
+}
+else{
+    
+    $metadata | ConvertTo-Json
+}
