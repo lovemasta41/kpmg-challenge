21eb0744f329e40ed0a1fdcdde3120d204553723
Masta, Love <love.masta@zurich.com>
2023-04-20 06:53:19 +0100

    updated outputs.tf

--- three-tier-architecture/outputs.tf
+++ three-tier-architecture/outputs.tf
@@ -1,3 +1,15 @@
 output "primary_rds_endpoint" {
   value = aws_db_instance.primary_instance.endpoint
 }
+
+output "web_ec2_ips" {
+
+    value = [for instance in aws_autoscaling_group.web_asg.instances : aws_instance.instance.private_ip ]
+  
+}
+
+output "app_ec2_ips" {
+
+    value = [for instance in aws_autoscaling_group.ap_asg.instances : aws_instance.instance.private_ip ]
+  
+}
